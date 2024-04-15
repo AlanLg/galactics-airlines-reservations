e804adc7f7eeedc0c24c8d84e3026d484f91dc2a
AlanLg <alanilateur@gmail.com>
2024-04-15 15:47:33 +0200

    fixed issue with lombok and mapstruct

--- src/main/java/com/galactics/airlines/reservations/model/entity/Client.java
+++ src/main/java/com/galactics/airlines/reservations/model/entity/Client.java
@@ -2,8 +2,10 @@ package com.galactics.airlines.reservations.model.entity;
 
 import jakarta.persistence.Entity;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Entity
 public class Client extends Person {
     private String passportNumber;
